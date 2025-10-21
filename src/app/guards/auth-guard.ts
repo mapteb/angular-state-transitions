8cc3afa5a5c7b8b9d9fa6c3b29ce9eca3ec0c1fc
Nalla Senthilnathan <nrsenthil@gmail.com>
2025-10-21 17:14:20 -0400

    Update auth-guard.ts

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -5,28 +5,23 @@ import { TransitionsHelper } from '../transitions-helper/transitions-helper';
 export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot) => {
   const transitionsHelper = inject(TransitionsHelper);
-  const router = inject(Router);
   const currentUser = transitionsHelper.getLoggedInUser();
   const allowedRoles = route.data['allowedRoles'];
   if (currentUser) {
     if (allowedRoles && allowedRoles.includes(currentUser.role)) {
-      transitionsHelper.setUser(currentUser);
+      if (state.url.includes('page3')) {
+        transitionsHelper.page3UserFound();
+      } else if (state.url == 'adminhome') {
+        transitionsHelper.adminhomeUserFound();
+      } else {
+        transitionsHelper.homeUserFound();
+      }      
       return true;
     }
-    console.log(">> currentUser has no allowedroles: ", currentUser);
-    const message = "User " + currentUser.username + " has no access to " + route.url;
-
-
-    console.log(">> currentUser has no allowedroles: ", currentUser);
-    // return router.createUrlTree(['/login'], {
-    //   queryParams: { message: message },
-    //   queryParamsHandling: 'merge' // or 'preserve', 'merge' is often useful
-    // }); 
-    transitionsHelper.authenticationError.set(message);   
-    router.navigate(['/login'], { replaceUrl: true, skipLocationChange: false  });
-    // return router.createUrlTree(['/login'])
-    // return false;
+    const message = "User " + currentUser.username + " has no access to " + state.url;
+    transitionsHelper.userNotAuthorized(message);
+    return false;
   }
 
-  return router.createUrlTree(['/login']);
+  return transitionsHelper.userNotFound();
 };
